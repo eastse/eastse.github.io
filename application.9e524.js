9fc43216f44fcb67b5b901417031dff769e08a5b
East <20434885+eastse@users.noreply.github.com>
2025-08-02 00:09:04 +0800

    1

--- application.9e524.js
+++ application.9e524.js
@@ -14,7 +14,7 @@ System.register([], function (_export, _context) {
       _export("Application", Application = /*#__PURE__*/function () {
         function Application() {
           _classCallCheck(this, Application);
-          this.settingsPath = 'src/settings.205df.json';
+          this.settingsPath = 'src/settings.2401e.json';
           this.showFPS = false;
         }
         _createClass(Application, [{
