6d69204539ab90271e5936383cd698f4fd63d9fe
East <20434885+eastse@users.noreply.github.com>
2025-08-02 00:13:27 +0800

    1

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-System.register(["./application.9e524.js"], function (_export, _context) {
+System.register(["./application.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
